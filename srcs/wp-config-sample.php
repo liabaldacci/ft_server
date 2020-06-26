939635dfe76d590a3f00d50904c0875a3df958bf
Giulia Adoglio <52930389+liabaldacci@users.noreply.github.com>
2020-06-26 21:05:16 +0200

    Update wp-config-sample.php

--- srcs/wp-config-sample.php
+++ srcs/wp-config-sample.php
@@ -20,13 +20,13 @@
 
 // ** Configurações do MySQL - Você pode pegar estas informações com o serviço de hospedagem ** //
 /** O nome do banco de dados do WordPress */
-define('DB_NAME', 'nome_do_banco_de_dados_aqui');
+define('DB_NAME', 'wordpress_db');
 
 /** Usuário do banco de dados MySQL */
-define('DB_USER', 'nome_de_usuario_aqui');
+define('DB_USER', 'root');
 
 /** Senha do banco de dados MySQL */
-define('DB_PASSWORD', 'senha_aqui');
+define('DB_PASSWORD', '');
 
 /** Nome do host do MySQL */
 define('DB_HOST', 'localhost');
@@ -50,14 +50,14 @@ define('DB_COLLATE', '');
  *
  * @since 2.6.0
  */
-define('AUTH_KEY',         'coloque a sua frase única aqui');
-define('SECURE_AUTH_KEY',  'coloque a sua frase única aqui');
-define('LOGGED_IN_KEY',    'coloque a sua frase única aqui');
-define('NONCE_KEY',        'coloque a sua frase única aqui');
-define('AUTH_SALT',        'coloque a sua frase única aqui');
-define('SECURE_AUTH_SALT', 'coloque a sua frase única aqui');
-define('LOGGED_IN_SALT',   'coloque a sua frase única aqui');
-define('NONCE_SALT',       'coloque a sua frase única aqui');
+define('AUTH_KEY',         'V} *0l43L4[838(n1I2)?v/F21/-3k(^:Dk&C(WbFa`|+}xPqxB|&t|scyumcJ$>');
+define('SECURE_AUTH_KEY',  'J+hqS/q6nmBb}/|8fYHGuy8$=%J(jP;$e31)qR?#Oid-/[XiRYCcUGqgNi90Enbo');
+define('LOGGED_IN_KEY',    'rvj3X=(J&VyHN*E$-d+UmFXJo#S;:(~(x*.DvyoZUF3-Yj1:u5fDZA64nO(%?ZmP');
+define('NONCE_KEY',        ';Rj<9qh7Yp/Ql|N-S=Laf(!2fjc~V1u!I;dtf0dSJ,01.pW}KWYF}O<7&gmc&Jae');
+define('AUTH_SALT',        '_9Y{q0EwQo8Dg(,y7u)Qd2P8BCv{C|o?ssjbgtmRks+2G17>s:`-x4>sX?UINM#6');
+define('SECURE_AUTH_SALT', 'VOGM-$kLRx~ZofaLN16|kB+|Jk?FV8uN}z/W([}fXWih9fMcmBegX6r#OH2$`/ F');
+define('LOGGED_IN_SALT',   '7N7:A@0i$mYj%msyM;/<Wa%[)|k<0,/f=4nrU0[&lKqa1>UM$z0l6?x>C=|6>Kxx');
+define('NONCE_SALT',       '}5fS~2K,jAN-+Hl|EQt[j>EDJgCNK+Fub>nNaA-#(?q8AxJA@_L3`?e&26=Pdz0t');
 
 /**#@-*/
 
