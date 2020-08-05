9539989f557b1a6d509c060a8e5200bc7e8945d4
Giulia Adoglio <52930389+liabaldacci@users.noreply.github.com>
2020-08-06 00:30:29 +0200

    Create autoindex.sh

---
+++ srcs/autoindex.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+nginx_file = '/etc/nginx/sites-available/default'
+
+grep "autoindex on;" "$nginx_file" > /dev/null
+
+if [ "$?" = "0" ]; then
+	echo "Autoindex is currently ON. Turning it OFF"
+	sed -i 's/autoindex on/autoindex off/g' "$nginx_file"
+	autoindex = "OFF"; 
+
+else
+	echo "Autoindex is currently OFF. Turning it ON"
+	sed -i 's/autoindex on/autoindex off/g' "$nginx_file"
+	autoindex = "OFF";
+fi
+
+echo "Restarting Nginx."
+/etc/init.d/nginx restart
+echo "Autoindex is now $autoindex."
