b175bcb1051bdc036cea2b38227b6d77bafa2a6e
Giulia Adoglio <52930389+liabaldacci@users.noreply.github.com>
2020-08-06 00:36:58 +0200

    TUDO FUNCIONANDO

--- srcs/autoindex.sh
+++ srcs/autoindex.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-nginx_file = '/etc/nginx/sites-available/default'
+nginx_file='/etc/nginx/sites-available/default'
 
 grep "autoindex on;" "$nginx_file" > /dev/null
 
 if [ "$?" = "0" ]; then
 	echo "Autoindex is currently ON. Turning it OFF"
 	sed -i 's/autoindex on/autoindex off/g' "$nginx_file"
-	autoindex = "OFF"; 
+	autoindex="OFF"; 
 
 else
 	echo "Autoindex is currently OFF. Turning it ON"
-	sed -i 's/autoindex on/autoindex off/g' "$nginx_file"
-	autoindex = "OFF";
+	sed -i 's/autoindex off/autoindex on/g' "$nginx_file"
+	autoindex="ON";
 fi
 
 echo "Restarting Nginx."
